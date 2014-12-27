297cac186a48c2065bf32469b526ff538cd6eda4
Dominic Mortel <dxmmortel@gmail.com>
2014-12-27 14:42:53 -0800

    Added Display Message

--- app/src/main/java/com/dominicmortel/myapplication/DisplayMessageActivity.java
+++ app/src/main/java/com/dominicmortel/myapplication/DisplayMessageActivity.java
@@ -5,42 +5,35 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
-
-    public final static String EXTRA_MESSAGE = "com.dominicmortel.MyApplication.MESSAGE";
-
-
+public class DisplayMessageActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textView = new TextView(this);
+        textView.setTextSize(20);
+
+        textView.setText(message);
+
+
+        setContentView(textView);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_display_message, menu);
         return true;
     }
-
-    public void sendMessage(View view){
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-
-        String message = editText.getText().toString();
-
-        intent.putExtra(EXTRA_MESSAGE, message);
-
-        startActivity(intent);
-    }
-
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
